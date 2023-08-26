e74a37822eae7b60548d846e88ee40b6c0718155
cjk-magic <magicsof@naver.com>
2023-08-27 02:18:26 +0900

    update

--- ch20/kube-node-setup.sh
+++ ch20/kube-node-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-rc-update add sshd
-rc-service sshd start
+mkdir /run/openrc
 touch /run/openrc/softlevel
+rc-update add sshd default
+rc-status default
 rc-service sshd start
 
 minikube start --force
