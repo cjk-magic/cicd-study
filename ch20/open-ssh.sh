568ee6c52001d71c688d35ed478cefdce12216d3
cjk-magic <magicsof@naver.com>
2023-08-27 01:25:47 +0900

    update

--- ch20/open-ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-apk add --update --no-cache openssh openrc
-sed -ri 's/^#?PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config 
-sed -ri 's/UsePAM yes/#UsePAM yes/g'                    /etc/ssh/sshd_config  
-echo -n 'root:1234' | chpasswd
-touch /run/openrc/softlevel
-rc-update add sshd
-rc-service sshd start
