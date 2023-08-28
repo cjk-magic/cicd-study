8204eab436a93e1f91cff1af7782da67aab93308
cjk-magic <magicsof@naver.com>
2023-08-28 14:20:00 +0900

    update

--- ch20/kube-node-setup.sh
+++ ch20/kube-node-setup.sh
@@ -5,6 +5,9 @@ rc-update add sshd default
 rc-status default
 rc-service sshd start
 
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
 minikube start --force
 minikube kubectl get nodes
 # alias kubectl="minikube kubectl"
