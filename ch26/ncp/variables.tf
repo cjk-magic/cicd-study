c7af9c5b7a422a1e18b6673f541a6604a0dc5ac2
cjk <magicsof@naver.com>
2023-08-31 15:47:04 +0900

    update

---
+++ ch26/ncp/variables.tf
@@ -0,0 +1,17 @@
+variable name_scn01 {
+  default = "tf-scn01"
+}
+
+variable client_ip {
+  default = "221.146.63.213"
+}
+
+variable "access_key" { # export TF_VAR_access_key=...
+  default = "XHlOWGDEpFbjJ0iBVv4a"
+}
+
+variable "secret_key" { # export TF_VAR_secret_key=...
+  default = "WRyx4NV1U7YP3TCamNPIGFkEpgutBIpDcbrwVN7V"
+}
+
+
