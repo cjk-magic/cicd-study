9bab785d68b561129aa0836a5a4c03be5442b961
cjk-magic <139297954+cjk-magic@users.noreply.github.com>
2023-09-03 16:11:42 +0900

    Update variables.tf

--- ch24/demo2/variables.tf
+++ ch24/demo2/variables.tf
@@ -1,21 +1,23 @@
-variable name_ncp_login {
-  default = "cicd"
-}
-
-variable server_token {
-  default = "ncp-token"
-}
-
-variable client_ip {
-  default ="000.000.000.000"  #221.146.63.213"
-}
-
 variable "access_key" { # export TF_VAR_access_key=...
-  default = "xxx"
+  default = "XHlOWGDEpFbjJ0iBVv4a"
 }
 
 variable "secret_key" { # export TF_VAR_secret_key=...
-  default = "xxx"
+  default = "WRyx4NV1U7YP3TCamNPIGFkEpgutBIpDcbrwVN7V"
 }
 
+variable "region" {
+  default = "KR"
+}
 
+variable "server_name" {
+  default = "terraform-test"
+}
+
+variable "server_image_product_code" { # Centos 7.3 64 bit
+  default = "SPSW0LINUX000046"
+}
+
+variable "server_product_code" {
+  default = "SPSVRHICPU000001" # vCpu 2, Memory 4G, Disk 50GB
+}
