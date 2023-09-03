f0ee879d95ab498c84307b2ef5833867d6153bd9
cjk-magic <magicsof@naver.com>
2023-09-03 14:24:45 +0900

    update

--- ch23/demo3/main.tf
+++ ch23/demo3/main.tf
@@ -9,16 +9,24 @@ terraform {
 
 provider "docker" {}
 
+data "docker_registry_image" "jenkins" {
+  name = "jenkins/jenkins:lts-jdk11"
+}
+
 resource "docker_image" "jenkins" {
-  name         = "jenkins:lts-jdk11"
-  keep_locally = false
+  name          = data.docker_registry_image.jenkins.name
+  pull_triggers = [data.docker_registry_image.jenkins.sha256_digest]
 }
 
 resource "docker_container" "jenkins-server" {
   image = docker_image.jenkins.image_id
-  name  = "tutorial"
+  name  = "jenkins-server"
   ports {
     internal = 8080
     external = 8080
+  }
+  ports {
+    internal = 50000
+    external = 50000
   }
 }
