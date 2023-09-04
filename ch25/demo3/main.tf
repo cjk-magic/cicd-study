42959a7c5f4fcf425a9b767a5a0dab2294233dbb
cjk-magic <139297954+cjk-magic@users.noreply.github.com>
2023-09-04 17:58:25 +0900

    Update main.tf

--- ch25/demo3/main.tf
+++ ch25/demo3/main.tf
@@ -1,29 +1,86 @@
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 3.0.1"
-    }
+provider "ncloud" {
+  support_vpc = true
+  region      = "KR"
+  access_key  = var.access_key
+  secret_key  = var.secret_key
+}
+
+resource "ncloud_vpc" "vpc" {
+    ipv4_cidr_block = "10.0.0.0/16"
+}
+
+resource "ncloud_subnet" "pub-sub" {  
+  vpc_no         = ncloud_vpc.vpc.id
+  subnet         = "10.0.1.0/24"
+  zone           = "KR-2"
+  network_acl_no = ncloud_vpc.vpc.default_network_acl_no
+  subnet_type    = "PUBLIC"
+}
+
+resource "ncloud_public_ip" "public_ip" {
+  server_instance_no = ncloud_server.server.id
+}
+
+resource "ncloud_login_key" "key" {
+  key_name = "ncp-token-key2"
+}
+
+resource "ncloud_server" "server" {
+  subnet_no                 = ncloud_subnet.pub-sub.id
+  name                      = "my-tf-server"
+  server_image_product_code = "SW.VSVR.OS.LNX64.UBNTU.SVR2004.B050"
+  login_key_name            = ncloud_login_key.key.key_name
+}
+
+data "ncloud_root_password" "pwd" {
+  server_instance_no = ncloud_server.server.id 
+  private_key = ncloud_login_key.key.private_key
+}
+
+resource "null_resource" "host_provisioner" {
+
+  provisioner "file" {
+      source = "script/"
+      destination = "/root/"
+      connection {
+        type = "ssh"
+        user = "root"
+        password = data.ncloud_root_password.pwd.root_password
+        host = ncloud_public_ip.public_ip.public_ip
+      }
+  }
+
+  provisioner "remote-exec" {
+      inline = [
+        "/bin/sh docker-setup.sh"
+      ]
+      connection {
+         type = "ssh"
+        user = "root"
+        password = data.ncloud_root_password.pwd.root_password
+        host = ncloud_public_ip.public_ip.public_ip
+      }
   }
 }
 
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
+output "cn_host_pw" {
+  sensitive = true
+  value = "sshpass -p '${data.ncloud_root_password.pwd.root_password}' ssh root@${ncloud_public_ip.public_ip.public_ip} -oStrictHostKeyChecking=no"
 }
 
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.image_id
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
 
-  provisioner "local-exec" {
-    command = "echo $(uname) >> os-version.txt"
-  }
-}
+
+#   provisioner "file" {
+#     source = "script/"
+#     destination = "/root/"
+#     connection {
+#       type = "ssh"
+#       user = "root"
+#       password = ncloud_root_password.
+#       host = self.public_ip
+#     }
+#   }
+
+
+# }
 
